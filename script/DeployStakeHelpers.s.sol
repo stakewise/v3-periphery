19567ab9e78c4264d325d574bbf60f4a4db8841c
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2024-10-22 17:17:18 +0100

    Fix spdx licenses

--- script/DeployStakeHelpers.s.sol
+++ script/DeployStakeHelpers.s.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: AGPL-3.0-only
 
 pragma solidity ^0.8.26;
 
@@ -36,7 +36,4 @@ contract DeployStakeHelpers is Script {
 
         vm.stopBroadcast();
     }
-
-    // excludes this contract from coverage report
-    function test() public {}
 }
