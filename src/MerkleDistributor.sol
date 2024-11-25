16b51fe2a87324355dcd83f4a1bd58225fe66e08
Dmitri Tsumak <tsumak.dmitri@gmail.com>
2024-11-25 23:52:18 +0400

    Implement MerkleDistributor

---
+++ src/MerkleDistributor.sol
@@ -0,0 +1,241 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+pragma solidity ^0.8.26;
+
+import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import {ECDSA} from '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
+import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+import {MerkleProof} from '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
+import {EIP712} from '@openzeppelin/contracts/utils/cryptography/EIP712.sol';
+import {Ownable2Step, Ownable} from '@openzeppelin/contracts/access/Ownable2Step.sol';
+import {Errors} from '@stakewise-core/libraries/Errors.sol';
+import {IKeeperOracles} from '@stakewise-core/interfaces/IKeeperOracles.sol';
+import {IMerkleDistributor} from './interfaces/IMerkleDistributor.sol';
+
+/**
+ * @title MerkleDistributor
+ * @author StakeWise
+ * @notice Distributes additional incentives using the Merkle tree.
+ */
+contract MerkleDistributor is Ownable2Step, EIP712, IMerkleDistributor {
+    uint256 private constant _signatureLength = 65;
+    bytes32 private constant _rewardsUpdateTypeHash =
+        keccak256('MerkleDistributor(bytes32 rewardsRoot,string rewardsIpfsHash,uint64 nonce)');
+
+    IKeeperOracles private immutable _keeper;
+
+    mapping(address token => mapping(address user => uint256 cumulativeAmount)) public claimedAmounts;
+
+    /// @inheritdoc IMerkleDistributor
+    bytes32 public rewardsRoot;
+
+    /// @inheritdoc IMerkleDistributor
+    uint64 public rewardsDelay;
+
+    /// @inheritdoc IMerkleDistributor
+    uint64 public rewardsMinOracles;
+
+    /// @inheritdoc IMerkleDistributor
+    uint64 public lastUpdateTimestamp;
+
+    /// @inheritdoc IMerkleDistributor
+    uint64 public nonce;
+
+    /**
+     * @dev Constructor
+     * @param keeper The address of the Keeper contract
+     * @param _initialOwner The address of the contract owner
+     * @param _rewardsDelay The delay in seconds before the rewards can be updated
+     * @param _rewardsMinOracles The minimum number of oracles required to update the rewards
+     */
+    constructor(
+        address keeper,
+        address _initialOwner,
+        uint64 _rewardsDelay,
+        uint64 _rewardsMinOracles
+    ) Ownable(msg.sender) EIP712('MerkleDistributor', '1') {
+        _keeper = IKeeperOracles(keeper);
+        setRewardsDelay(_rewardsDelay);
+        setRewardsMinOracles(_rewardsMinOracles);
+        _transferOwnership(_initialOwner);
+    }
+
+    /// @inheritdoc IMerkleDistributor
+    function setRewardsRoot(
+        bytes32 newRewardsRoot,
+        string calldata newRewardsIpfsHash,
+        bytes calldata signatures
+    ) external {
+        // check whether merkle root is not zero or the same as current
+        if (newRewardsRoot == bytes32(0) || newRewardsRoot == rewardsRoot) {
+            revert Errors.InvalidRewardsRoot();
+        }
+        // check whether rewards delay has passed
+        if (lastUpdateTimestamp + rewardsDelay > block.timestamp) {
+            revert Errors.TooEarlyUpdate();
+        }
+
+        // verify rewards update signatures
+        _verifySignatures(
+            rewardsMinOracles,
+            keccak256(abi.encode(_rewardsUpdateTypeHash, newRewardsRoot, keccak256(bytes(newRewardsIpfsHash)), nonce)),
+            signatures
+        );
+
+        // update state
+        rewardsRoot = newRewardsRoot;
+        // cannot overflow on human timescales
+        lastUpdateTimestamp = uint64(block.timestamp);
+        nonce += 1;
+
+        // emit event
+        emit RewardsRootUpdated(msg.sender, newRewardsRoot, newRewardsIpfsHash);
+    }
+
+    /// @inheritdoc IMerkleDistributor
+    function setRewardsDelay(
+        uint64 _rewardsDelay
+    ) public onlyOwner {
+        rewardsDelay = _rewardsDelay;
+        emit RewardsDelayUpdated(msg.sender, _rewardsDelay);
+    }
+
+    function setRewardsMinOracles(
+        uint64 _rewardsMinOracles
+    ) public onlyOwner {
+        if (_rewardsMinOracles == 0 || _keeper.totalOracles() < _rewardsMinOracles) {
+            revert Errors.InvalidOracles();
+        }
+        rewardsMinOracles = _rewardsMinOracles;
+        emit RewardsMinOraclesUpdated(msg.sender, _rewardsMinOracles);
+    }
+
+    /// @inheritdoc IMerkleDistributor
+    function distributePeriodically(
+        address token,
+        uint256 amount,
+        uint256 delayInSeconds,
+        uint256 durationInSeconds,
+        bytes calldata extraData
+    ) external onlyOwner {
+        if (amount == 0) revert InvalidAmount();
+        if (durationInSeconds == 0) revert InvalidDuration();
+
+        SafeERC20.safeTransferFrom(IERC20(token), msg.sender, address(this), amount);
+        emit PeriodicDistributionAdded(msg.sender, token, amount, delayInSeconds, durationInSeconds, extraData);
+    }
+
+    /// @inheritdoc IMerkleDistributor
+    function distributeOneTime(
+        address token,
+        uint256 amount,
+        string calldata rewardsIpfsHash,
+        bytes calldata extraData
+    ) external override onlyOwner {
+        if (amount == 0) revert InvalidAmount();
+
+        SafeERC20.safeTransferFrom(IERC20(token), msg.sender, address(this), amount);
+        emit OneTimeDistributionAdded(msg.sender, token, amount, rewardsIpfsHash, extraData);
+    }
+
+    /// @inheritdoc IMerkleDistributor
+    function claim(
+        address account,
+        address[] calldata tokens,
+        uint256[] calldata cumulativeAmounts,
+        bytes32[] calldata merkleProof
+    ) external {
+        if (account == address(0)) revert Errors.ZeroAddress();
+        uint256 tokensCount = tokens.length;
+        if (tokensCount == 0 || tokensCount != cumulativeAmounts.length) revert InvalidTokens();
+
+        // SLOAD to memory
+        bytes32 merkleRoot = rewardsRoot;
+
+        // verify the merkle proof
+        if (
+            !MerkleProof.verifyCalldata(
+                merkleProof,
+                merkleRoot,
+                keccak256(bytes.concat(keccak256(abi.encode(tokens, account, cumulativeAmounts))))
+            )
+        ) {
+            revert Errors.InvalidProof();
+        }
+
+        uint256 amount;
+        address token;
+        address lastToken;
+        uint256[] memory transfers = new uint256[](tokensCount);
+        for (uint256 i = 0; i < tokensCount;) {
+            token = tokens[i];
+            // tokens must be sorted and not repeat
+            if (token <= lastToken) revert InvalidTokens();
+
+            // calculate the amount to transfer
+            amount = cumulativeAmounts[i];
+            transfers[i] = amount - claimedAmounts[token][account];
+
+            // update state
+            claimedAmounts[token][account] = amount;
+            lastToken = token;
+            unchecked {
+                i++;
+            }
+        }
+
+        // send the tokens
+        for (uint256 i = 0; i < tokensCount;) {
+            token = tokens[i];
+            amount = transfers[i];
+            if (amount > 0) {
+                SafeERC20.safeTransfer(IERC20(token), account, amount);
+            }
+            unchecked {
+                i++;
+            }
+        }
+        emit RewardsClaimed(msg.sender, account, tokens, cumulativeAmounts);
+    }
+
+    /**
+     * @notice Internal function for verifying oracles' signatures
+     * @param requiredSignatures The number of signatures required for the verification to pass
+     * @param message The message that was signed
+     * @param signatures The concatenation of the oracles' signatures
+     */
+    function _verifySignatures(uint256 requiredSignatures, bytes32 message, bytes calldata signatures) private view {
+        if (requiredSignatures == 0) revert Errors.InvalidOracles();
+
+        // check whether enough signatures
+        unchecked {
+            // cannot realistically overflow
+            if (signatures.length < requiredSignatures * _signatureLength) {
+                revert Errors.NotEnoughSignatures();
+            }
+        }
+
+        bytes32 data = _hashTypedDataV4(message);
+        address lastOracle;
+        address currentOracle;
+        uint256 startIndex;
+        for (uint256 i = 0; i < requiredSignatures; i++) {
+            unchecked {
+                // cannot overflow as signatures.length is checked above
+                currentOracle = ECDSA.recover(data, signatures[startIndex:startIndex + _signatureLength]);
+            }
+            // signatures must be sorted by oracles' addresses and not repeat
+            if (currentOracle <= lastOracle || !_keeper.isOracle(currentOracle)) {
+                revert Errors.InvalidOracle();
+            }
+
+            // update last oracle
+            lastOracle = currentOracle;
+
+            unchecked {
+                // cannot realistically overflow
+                startIndex += _signatureLength;
+            }
+        }
+    }
+}
---
+++ src/interfaces/IMerkleDistributor.sol
@@ -0,0 +1,186 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+pragma solidity ^0.8.26;
+
+/**
+ * @title IMerkleDistributor
+ * @author StakeWise
+ * @notice Defines the interface for the MerkleDistributor contract
+ */
+interface IMerkleDistributor {
+    error InvalidTokens();
+    error InvalidAmount();
+    error InvalidDuration();
+
+    /**
+     * @notice Emitted when the rewards root is updated
+     * @param caller The address of the caller
+     * @param newRewardsRoot The new rewards Merkle Tree root
+     * @param newRewardsIpfsHash The new rewards IPFS hash
+     */
+    event RewardsRootUpdated(address indexed caller, bytes32 indexed newRewardsRoot, string newRewardsIpfsHash);
+
+    /**
+     * @notice Emitted when the rewards delay is updated
+     * @param caller The address of the caller
+     * @param newRewardsDelay The new rewards delay
+     */
+    event RewardsDelayUpdated(address indexed caller, uint64 newRewardsDelay);
+
+    /**
+     * @notice Emitted when the minimum number of oracles required to update the rewards root is updated
+     * @param caller The address of the caller
+     * @param newRewardsMinOracles The new minimum number of oracles required
+     */
+    event RewardsMinOraclesUpdated(address indexed caller, uint64 newRewardsMinOracles);
+
+    /**
+     * @notice Emitted when a periodic distribution is added
+     * @param caller The address of the caller
+     * @param token The address of the token
+     * @param amount The amount of tokens to distribute
+     * @param delayInSeconds The delay in seconds before the first distribution
+     * @param durationInSeconds The total duration of the distribution
+     * @param extraData The extra data for the distribution
+     */
+    event PeriodicDistributionAdded(
+        address indexed caller,
+        address indexed token,
+        uint256 amount,
+        uint256 delayInSeconds,
+        uint256 durationInSeconds,
+        bytes extraData
+    );
+
+    /**
+     * @notice Emitted when a one-time distribution is added
+     * @param caller The address of the caller
+     * @param token The address of the token
+     * @param amount The amount of tokens to distribute
+     * @param rewardsIpfsHash The IPFS hash of the rewards
+     * @param extraData The extra data for the distribution
+     */
+    event OneTimeDistributionAdded(
+        address indexed caller, address indexed token, uint256 amount, string rewardsIpfsHash, bytes extraData
+    );
+
+    /**
+     * @notice Emitted when the rewards are claimed
+     * @param account The address of the account
+     * @param tokens The list of tokens
+     * @param cumulativeAmounts The cumulative amounts of tokens
+     */
+    event RewardsClaimed(
+        address indexed caller, address indexed account, address[] tokens, uint256[] cumulativeAmounts
+    );
+
+    /**
+     * @notice Get the current rewards Merkle Tree root
+     * @return The current rewards Merkle Tree root
+     */
+    function rewardsRoot() external view returns (bytes32);
+
+    /**
+     * @notice Get the delay in seconds after which the rewards root can be updated
+     * @return The current rewards delay
+     */
+    function rewardsDelay() external view returns (uint64);
+
+    /**
+     * @notice Get the minimum number of oracles required to update the rewards root
+     * @return The current minimum number of oracles required
+     */
+    function rewardsMinOracles() external view returns (uint64);
+
+    /**
+     * @notice Get the timestamp of the last rewards root update
+     * @return The timestamp of the last rewards root update
+     */
+    function lastUpdateTimestamp() external view returns (uint64);
+
+    /**
+     * @notice Get the nonce used by oracles to sign the new rewards root update
+     * @return The nonce used for updating the rewards root
+     */
+    function nonce() external view returns (uint64);
+
+    /**
+     * @notice Get the cumulative claimed amount for a user
+     * @param token The address of the token
+     * @param user The address of the user
+     * @return cumulativeAmount The cumulative claimed amount for the user
+     */
+    function claimedAmounts(address token, address user) external view returns (uint256 cumulativeAmount);
+
+    /**
+     * @notice Set the new rewards root
+     * @param newRewardsRoot The new rewards Merkle Tree root
+     * @param newRewardsIpfsHash The new rewards IPFS hash
+     * @param signatures The signatures of the oracles
+     */
+    function setRewardsRoot(
+        bytes32 newRewardsRoot,
+        string calldata newRewardsIpfsHash,
+        bytes calldata signatures
+    ) external;
+
+    /**
+     * @notice Set the new rewards delay. Can only be called by the owner.
+     * @param newRewardsDelay The new rewards delay
+     */
+    function setRewardsDelay(
+        uint64 newRewardsDelay
+    ) external;
+
+    /**
+     * @notice Set the new minimum number of oracles required to update the rewards root. Can only be called by the owner.
+     * @param newRewardsMinOracles The new minimum number of oracles required
+     */
+    function setRewardsMinOracles(
+        uint64 newRewardsMinOracles
+    ) external;
+
+    /**
+     * @notice Distribute tokens every rewards delay for a specific duration
+     * @param token The address of the token
+     * @param amount The amount of tokens to distribute
+     * @param delayInSeconds The delay in seconds before the first distribution
+     * @param durationInSeconds The total duration of the distribution
+     * @param extraData The extra data for the distribution
+     */
+    function distributePeriodically(
+        address token,
+        uint256 amount,
+        uint256 delayInSeconds,
+        uint256 durationInSeconds,
+        bytes calldata extraData
+    ) external;
+
+    /**
+     * @notice Distribute tokens one time
+     * @param token The address of the token
+     * @param amount The amount of tokens to distribute
+     * @param rewardsIpfsHash The IPFS hash of the rewards
+     * @param extraData The extra data for the distribution
+     */
+    function distributeOneTime(
+        address token,
+        uint256 amount,
+        string calldata rewardsIpfsHash,
+        bytes calldata extraData
+    ) external;
+
+    /**
+     * @notice Claim the tokens for a user
+     * @param account The address of the user
+     * @param tokens The list of tokens
+     * @param cumulativeAmounts The cumulative amounts of tokens
+     * @param merkleProof The Merkle proof
+     */
+    function claim(
+        address account,
+        address[] calldata tokens,
+        uint256[] calldata cumulativeAmounts,
+        bytes32[] calldata merkleProof
+    ) external;
+}
